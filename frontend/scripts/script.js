54b37d33b940d61cb3349c2f28f282142e12652e
lolewka <lolewka@gmail.com>
2025-06-18 16:04:51 +0300

    add header,styles-header,script burger menu open and close

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,6 +1,12 @@
 const burger = document.getElementById("burger");
 const nav = document.getElementById("menu");
+const close = document.getElementById("close");
 
 burger.addEventListener('click', () => {
-    nav.classList.toggle("active");
+    nav.classList.add("active");
+})
+
+close.addEventListener('click', () => {
+    nav.classList.remove("active");
+    burger.classList.remove('active');
 })
